c2808db3139a0756ed271e85340c5f2142bc102f
viktor <viktorkarlsson125@gmail.com>
2021-05-06 00:00:45 +0200

    adding self-made link image

--- src/components/pages/what/ProjectCard.jsx
+++ src/components/pages/what/ProjectCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
-const ProjectCard = ({project}) => {
+const ProjectCard = ({ project }) => {
   return (
-    <ui>
+    <div
+      key={project.id}
+    >
       <h4>{project.name}</h4>
       <p>{project.description}</p>
-      <i class="fas fa-link">
-        {project.link}
-      </i>
+      <button className="project_image_button" href={project.link}>
+      <img src="/images/linkimg.png" alt="link" className="project_image"/>
+      </button>
       <p>{project.language}</p>
-    </ui>
+    </div>
   )
 }
 

f362bacd942507a12566578a600c612713f5e5f3
viktor <viktorkarlsson125@gmail.com>
2021-05-08 16:25:17 +0200

    still WIP styling projectCard

--- src/components/pages/what/ProjectCard.jsx
+++ src/components/pages/what/ProjectCard.jsx
@@ -3,22 +3,23 @@ import './What.css'
 
 const ProjectCard = ({ project }) => {
   return (
-    <div key={project.id} className="project_container">
-      <div className="grid">
+    <div key={project.id} className="main_card_container">
         <h4>{project.name}</h4>
-        <p>{project.description}</p>
+      <div className="card_container">
+        {/* <p>{project.description}</p>
         {project.link ? (
           <div onClick={() => window.open(`${project.link}`)}>
             <button className="project_button">
               <img
                 src="/images/link_red.png"
-                alt="link"
+                alt="Link"
                 className="project_image"
               />
             </button>
           </div>
         ) : null}
-        <p>{project.language}</p>
+        <p>{project.language}</p> */}
+        <p>{project.id}</p>
       </div>
     </div>
   )

700e0c7b54fe5c8b3f23ce6635714ef848bff964
viktor <viktorkarlsson125@gmail.com>
2021-05-10 18:25:02 +0200

    adding content and style to how page

--- src/components/pages/how/How.jsx
+++ src/components/pages/how/How.jsx
@@ -4,8 +4,16 @@ import '../GlobalPageStyle.css'
 
 const How = () => {
   return (
-    <div className="main_page_container">
-      <h1>this is the How page</h1>
+    <div className="main_how_container">
+      <h1>Contact</h1>
+      <h4>Email: Viktorkarlsson125@gmail.com</h4>
+      <h4>Phone: 073 370 67 33</h4>
+      <br/>
+      <h2>Links</h2>
+      <div className="how_link_container">
+        <h4 className="how_link" onClick={() => window.open("https://github.com/ViktorHek")}>GitHub</h4>
+        <h4 className="how_link" onClick={() => window.open("https://www.linkedin.com/in/viktor-karlsson-7ba676a3/")}>LinkedIn</h4>
+      </div>
     </div>
   )
 }

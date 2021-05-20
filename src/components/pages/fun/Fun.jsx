e99b07a55f1c74b56dd4008da990f06d5cc78c43
viktor <viktorkarlsson125@gmail.com>
2021-05-20 13:11:09 +0200

    creating fun_page with route

---
+++ src/components/pages/fun/Fun.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Fun = () => {
+  return (
+    <div>
+      <h1>
+        I like to have fun. Hope you do to. I want this page to be more then
+        just a presentation of me but also a place you can stay an have fun.
+      </h1>
+      <h3>At the moment I can only offer you a rock, paper, scissor game</h3>
+    </div>
+  )
+}
+
+export default Fun
